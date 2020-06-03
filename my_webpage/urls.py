6bceac1c5494f620242935c72ba7c040f78be2f8
rcmschiavi <rodolfocmschiavi@gmail.com>
2020-06-03 15:05:48 -0400

    Changing views and urls on main aplication

--- my_webpage/urls.py
+++ my_webpage/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_pages.urls')),
-    path('application', include('applications.urls')),
-    path('blog', include('blog_dev.urls')),
-    path('application', include('applications.urls'))
+    path('application/', include('applications.urls')),
+    path('blog/', include('blog_dev.urls')),
+    path('application/', include('applications.urls'))
 ]
