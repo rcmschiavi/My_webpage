e9d7b8b9664c6add5f63fa6235c9d2a65b2e41f3
rcmschiavi <rodolfocmschiavi@gmail.com>
2020-06-03 15:05:24 -0400

    Implementing post_temperature function on views

--- applications/urls.py
+++ applications/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/lala', views.index, name='index')
+    url('temperature_data', views.post_temperature, name='post_temp')
 ]
