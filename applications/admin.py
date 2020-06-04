1f3228dfd1c8754a1968a6b4cc459b3da51f1943
rcmschiavi <rodolfocmschiavi@gmail.com>
2020-06-04 10:27:55 -0400

    Admin panel

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from applications.models import Temperature, My_plant_data
 
-# Register your models here.
+admin.site.register(Temperature)
+admin.site.register(My_plant_data)
