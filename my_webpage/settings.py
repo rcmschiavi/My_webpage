b96d800e4445f690a6383f0123c61a52a40333c2
rcmschiavi <rodolfocmschiavi@gmail.com>
2021-04-17 15:37:59 -0400

    :card_file_box: adding database to run locally

--- my_webpage/settings.py
+++ my_webpage/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'my_webpage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'all_data',
+    'USER': 'rodolfoSchiavi',
+    'PASSWORD': 'easy_password',
+    'HOST': 'localhost',
     }
 }
 
